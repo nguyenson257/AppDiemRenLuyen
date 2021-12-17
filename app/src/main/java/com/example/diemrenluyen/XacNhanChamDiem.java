39e227e3f1a4598faf5fde227cec77c2eecd8a38
nguyenson257 <nguyenson25701@gmail.com>
2021-12-17 11:12:49 +0700

    Son up giao dien

---
+++ app/src/main/java/com/example/diemrenluyen/XacNhanChamDiem.java
@@ -0,0 +1,14 @@
+package com.example.diemrenluyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class XacNhanChamDiem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xac_nhan_cham_diem);
+    }
+}
