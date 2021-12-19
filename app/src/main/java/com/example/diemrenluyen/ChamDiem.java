d35219603cf866d8912ccfbab9e7a4503be60196
nguyenson257 <nguyenson25701@gmail.com>
2021-12-19 22:03:30 +0700

    uptest

--- app/src/main/java/com/example/diemrenluyen/ChamDiem.java
+++ app/src/main/java/com/example/diemrenluyen/ChamDiem.java
@@ -53,41 +53,43 @@ public class ChamDiem extends AppCompatActivity {
              ) {
             tongdiem = tongdiem + d.diemcham;
         };
-        tongdiem1.setText(String.valueOf(tongdiem));
+        String text = "Tổng điểm mục 1: "+(tongdiem)+"/"+(mmax1)+" điểm";
+        tongdiem1.setText(text);
     }
 
     private void mucchamlist(){
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         danhsachmuccham.setLayoutManager(linearLayoutManager);
-        ArrayList<mucchamdomain> mucchamdomains = new ArrayList<>();
-//        ApiService.apiService.showitem().enqueue(new Callback<ArrayList<mucchamdomain>>() {
-//            @Override
-//            public void onResponse(Call<ArrayList<mucchamdomain>> call, Response<ArrayList<mucchamdomain>> response) {
-////                Toast.makeText(TrangChu.this,khachhang.toString(), Toast.LENGTH_SHORT).show();
-//                ArrayList<mucchamdomain> mucchamdomain = response.body();
-//                adapter = new mucdiemAdapter(mucchamdomain);
-//                danhsachmuccham.setAdapter(adapter);
-//            }
+//        ArrayList<mucchamdomain> mucchamdomains = new ArrayList<>();
+        ApiService.apiService.showitem().enqueue(new Callback<ArrayList<mucchamdomain>>() {
+            @Override
+            public void onResponse(Call<ArrayList<mucchamdomain>> call, Response<ArrayList<mucchamdomain>> response) {
+//                Toast.makeText(TrangChu.this,khachhang.toString(), Toast.LENGTH_SHORT).show();
+                ArrayList<mucchamdomain> mucchamdomains = response.body();
+                for (mucchamdomain i:mucchamdomains
+                ) {
+                    mmax1 =mmax1+ i.getDiemtoida();
+                }
+                adapter = new mucdiemAdapter(mucchamdomains);
+                danhsachmuccham.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<mucchamdomain>> call, Throwable t) {
+
+            }
+        });
+//        mucchamdomains.add(new mucchamdomain(1,1,"Noi dung 1",3));
+//        mucchamdomains.add(new mucchamdomain(2,1,"Noi dung 2",3));
+//        mucchamdomains.add(new mucchamdomain(3,1,"Noi dung 3",4));
+//        mucchamdomains.add(new mucchamdomain(4,1,"Noi dung 4",2));
+//        mucchamdomains.add(new mucchamdomain(5,1,"Noi dung 5",3));
+//        mucchamdomains.add(new mucchamdomain(6,1,"Noi dung 6",4));
+//        mucchamdomains.add(new mucchamdomain(7,1,"Noi dung 7",1));
+//        mucchamdomains.add(new mucchamdomain(8,1,"Noi dung 8",2));
 //
-//            @Override
-//            public void onFailure(Call<ArrayList<mucchamdomain>> call, Throwable t) {
-//
-//            }
-//        });
-        mucchamdomains.add(new mucchamdomain(1,1,"Noi dung 1",3));
-        mucchamdomains.add(new mucchamdomain(2,1,"Noi dung 2",3));
-        mucchamdomains.add(new mucchamdomain(3,1,"Noi dung 3",4));
-        mucchamdomains.add(new mucchamdomain(4,1,"Noi dung 4",2));
-        mucchamdomains.add(new mucchamdomain(5,1,"Noi dung 5",3));
-        mucchamdomains.add(new mucchamdomain(6,1,"Noi dung 6",4));
-        mucchamdomains.add(new mucchamdomain(7,1,"Noi dung 7",1));
-        mucchamdomains.add(new mucchamdomain(8,1,"Noi dung 8",2));
-        for (mucchamdomain i:mucchamdomains
-             ) {
-            mmax1 =mmax1+ i.getDiemtoida();
-        }
-        adapter = new mucdiemAdapter(mucchamdomains,listdiem);
-        danhsachmuccham.setAdapter(adapter);
+//        adapter = new mucdiemAdapter(mucchamdomains,listdiem);
+//        danhsachmuccham.setAdapter(adapter);
     }
     public void init(){
         danhsachmuccham = findViewById(R.id.danhsach1);
