10e8ecffd3699b87e946e21435ae887d5a3e62f3
duongmien <duongthimien7@gmail.com>
2021-12-26 13:07:06 +0700

    update

--- app/src/main/java/com/example/diemrenluyen/XemDiem.java
+++ app/src/main/java/com/example/diemrenluyen/XemDiem.java
@@ -11,6 +11,7 @@ import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.icu.lang.UCharacter;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
@@ -60,7 +61,7 @@ public class XemDiem extends AppCompatActivity {
         mic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                speech_TO_TEXT();
+                requestAudioPermissions();
             }
         });
         search_box.addTextChangedListener(new TextWatcher() {
@@ -98,6 +99,64 @@ public class XemDiem extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),
                     "Sorry! Your device doesn\\'t support speech input" ,
                     Toast.LENGTH_SHORT).show();
+        }
+    }
+    //Requesting run-time permissions
+
+    //Create placeholder for user's consent to record_audio permission.
+//This will be used in handling callback
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+    private void requestAudioPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+            //When permission is not granted by user, show them message why this permission is needed.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show();
+
+                //Give user option to still opt-in the permissions
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+
+            } else {
+                // Show user dialog to grant permission to record audio
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+            }
+        }
+        //If permission is granted, then go ahead recording audio
+        else if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            //Go ahead with recording audio now
+            speech_TO_TEXT();
+        }
+    }
+
+    //Handling callback
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_RECORD_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+                    speech_TO_TEXT();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permissions Denied to record audio", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
         }
     }
     /**
